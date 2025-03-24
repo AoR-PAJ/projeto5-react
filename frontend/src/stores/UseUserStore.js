ede12178056849e816953cf6325b97291d9fff46
VinicyusFortes <vinicyusfortess@gmail.com>
2025-03-24 11:05:07 +0000

    UserStore alterado para UseUserStore

--- frontend/src/stores/UseUserStore.js
+++ frontend/src/stores/UseUserStore.js
@@ -1,19 +1,19 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
-
-export const UserStore = create(
+export const UseUserStore = create(
   persist(
     (set) => ({
-      firstName:"John" ,
+      firstName: "John",
       lastName: "Doe",
       username: "johndoe",
       email: "john@gmail.com",
       phone: "123456789",
-      photo: "imagem"
-    }), {
-      name: "userStore",
-      storage:createJSONStorage(()=>sessionStorage),
+      photo: "imagem",
+    }),
+    {
+      name: "UseUserStore",
+      storage: createJSONStorage(() => sessionStorage),
     }
   )
-)
+);
