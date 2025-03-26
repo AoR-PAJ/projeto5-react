082f54d5c056c29b5c01e1df02b62110f4b79670
VinicyusFortes <vinicyusfortess@gmail.com>
2025-03-26 17:45:46 +0000

    refactor services.jsx e ueAuthStore: inclusao do metodo de logout

--- frontend/src/components/buttons/logout/Logout.jsx
+++ frontend/src/components/buttons/logout/Logout.jsx
@@ -9,39 +9,21 @@ const LogoutBtn = () => {
   const logout = useAuthStore((state) => state.logout);
   const username = useAuthStore((state) => state.username);
 
-  const handleLogout = () => {
+  const handleLogout = async () => {
     const token = sessionStorage.getItem("token");
 
-    if (token) {
-      fetch("http://localhost:8080/vanessa-vinicyus-proj3/rest/users/logout", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          Accept: "application/json",
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) {
-            sessionStorage.removeItem("token");
+    if(token) {
+      const success = await logout(token);
 
-            //atualiza o estado global
-            logout();
-
-            //feedback
-            alert("Até a próxima!");
-
-            //redireciona
-            navigate("/homePage");
-          } else {
-            alert("Erro ao fazer logout!");
-          }
-        })
-        .catch((error) => {
-          console.error("Erro ao realizar logout ", error);
-          alert("Ocorreu um erro, tente novamente.");
-        });
+      if(success) {
+        sessionStorage.removeItem("token");
+        alert("Até a próxima!");
+        navigate("/homePage");
+      } else {
+        alert("Erro ao realizar logout!");
+      }
     }
-  };
+   };
 
   //exibe o botao de logout apenas se o user estiver conectado
   if (username) {
--- frontend/src/stores/UseAuthStore.js
+++ frontend/src/stores/UseAuthStore.js
@@ -2,7 +2,6 @@ import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 import { Service } from "../Services/Services";
 
-// Define the store
 export const useAuthStore = create(
   persist(
     (set) => ({
@@ -10,25 +9,15 @@ export const useAuthStore = create(
       profilePicture: "imagem",
       admin: false,
 
-      // Função para atualizar o nome de usuário
       updateName: (username) => set({ username }),
-
-      // Função para atualizar a foto de perfil
       updatePhoto: (profilePicture) => set({ profilePicture }),
-
-      // Função para atualizar o status de admin
       updateAdmin: (admin) => set({ admin }),
-
-      // Função para limpar o estado após o logout
-      logout: () => set({ username: "", profilePicture: "", admin: false }),
-
-      // Função para fazer o logout via camada de serviço
-      logoutAuthStore: async (token) => {
+      logout: async (token) => {
         try {
-          // Chama a função do serviço para fazer a requisição de logout
+          
           const result = await Service.logout(token);
           if (result) {
-            // Se o logout for bem-sucedido, chama a função logout da store para limpar os dados
+            // faz reset das credenciais
             set({ username: "", profilePicture: "", admin: false });
             return true; 
           }
