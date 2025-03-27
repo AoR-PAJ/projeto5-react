f897221473dc8c0c586521ea5f6b882998bf3854
VinicyusFortes <vinicyusfortess@gmail.com>
2025-03-27 14:58:07 +0000

    update ProductsActions.js: adicionados testes

--- frontend/src/tests/ProductsActions.test.js
+++ frontend/src/tests/ProductsActions.test.js
@@ -49,3 +49,51 @@ it("deve chamar a função onEdit quando o botão Edit Product for clicado", () 
   // Verificando se a função mockada foi chamada
   expect(mockOnEdit).toHaveBeenCalledTimes(1);
 });
+
+it("deve exibir o botão de Buy quando o usuário não for dono e não for admin", () => {
+  const mockOnEdit = jest.fn();
+  const mockOnDelete = jest.fn();
+  const mockOnBuy = jest.fn();
+
+  // Renderizando o componente com isOwner=false e isAdmin=false
+  render(
+    <ProductActions
+      isOwner={false}
+      isAdmin={false}
+      onEdit={mockOnEdit}
+      onDelete={mockOnDelete}
+      onBuy={mockOnBuy}
+    />
+  );
+
+  // Verificando se o botão de "Buy" está sendo renderizado
+  expect(screen.getByText("Buy")).toBeInTheDocument();
+
+  // Verificando se os botões "Edit Product" e "Delete Product" não estão presentes
+  expect(screen.queryByText("Edit Product")).not.toBeInTheDocument();
+  expect(screen.queryByText("Delete Product")).not.toBeInTheDocument();
+});
+
+
+it("deve exibir os botões de Buy, Edit Product e Delete Product quando o usuário não for dono mas for admin", () => {
+  const mockOnEdit = jest.fn();
+  const mockOnDelete = jest.fn();
+  const mockOnBuy = jest.fn();
+
+  // Renderizando o componente com isOwner=false e isAdmin=true
+  render(
+    <ProductActions
+      isOwner={false}
+      isAdmin={true}
+      onEdit={mockOnEdit}
+      onDelete={mockOnDelete}
+      onBuy={mockOnBuy}
+    />
+  );
+
+  // Verificando se os botões "Buy", "Edit Product" e "Delete Product" estão sendo renderizados
+  expect(screen.getByText("Buy")).toBeInTheDocument();
+  expect(screen.getByText("Edit Product")).toBeInTheDocument();
+  expect(screen.getByText("Delete Product")).toBeInTheDocument();
+});
+
