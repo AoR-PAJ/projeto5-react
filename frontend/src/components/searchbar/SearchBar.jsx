568fea4df4615b7b3026f0ccb4ee040b880db679
VinicyusFortes <vinicyusfortess@gmail.com>
2025-04-18 11:22:10 +0100

    responsividade searchbar

--- frontend/src/components/searchbar/SearchBar.jsx
+++ frontend/src/components/searchbar/SearchBar.jsx
@@ -1,23 +1,27 @@
 import { FormattedMessage } from "react-intl";
-import {useIntl} from "react-intl";
+import { useIntl } from "react-intl";
+import "./SearchBar.css";
 
 const SearchBar = () => {
   const intl = useIntl();
 
   return (
-  <div id="search-bar-div">
-    <input
-      type="text"
-      id="search-input"
-      className="search-bar"
-      placeholder={intl.formatMessage({ id: "searchbar.placeholder" })}
-    />
-    <button id="search-button" className="btn btn-success">
-      <FormattedMessage id="searchbutton.text" />
-    </button>
-  </div>
-  )
-}
-;
+    <div id="search-bar-div" className="container my-3">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-8 col-lg-6 d-flex">
+          <input
+            type="text"
+            id="search-input"
+            className="form-control me-2"
+            placeholder={intl.formatMessage({ id: "searchbar.placeholder" })}
+          />
+          <button id="search-button" className="btn btn-success">
+            <FormattedMessage id="searchbutton.text" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
