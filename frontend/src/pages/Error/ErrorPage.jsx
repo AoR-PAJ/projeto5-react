b3d746ea8ab9934f8d9eeb2d5062895de1e52db4
VinicyusFortes <vinicyusfortess@gmail.com>
2025-04-18 13:56:14 +0100

    responsividade pagina de erro

--- frontend/src/pages/Error/ErrorPage.jsx
+++ frontend/src/pages/Error/ErrorPage.jsx
@@ -3,19 +3,20 @@ import "./ErrorPage.css";
 import { useNavigate } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
 
-//Esta página será exibida caso uma rota que nao faça parte do router seja acessada
+// Esta página será exibida caso uma rota que não faça parte do router seja acessada
 function ErrorPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="error-page">
+    <div className="error-page d-flex flex-column align-items-center justify-content-center vh-100">
       <img
         src="https://img.freepik.com/vetores-gratis/ups-erro-404-com-ilustracao-de-conceito-de-robo-quebrado_114360-5529.jpg"
         alt="error image"
+        className="error-image img-fluid"
         onClick={() => navigate("/homePage")}
       />
 
-      <div>
+      <div className="text-center mt-4">
         <h1>
           404 - <FormattedMessage id="pageNotFound" />
         </h1>
