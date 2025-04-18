b151cecbadd23289cb97c13e928bbb319e2061cf
VinicyusFortes <vinicyusfortess@gmail.com>
2025-04-18 11:17:13 +0100

    responsividade banner

--- frontend/src/components/banner/Banner.jsx
+++ frontend/src/components/banner/Banner.jsx
@@ -1,16 +1,39 @@
+import React from "react";
 import { FormattedMessage } from "react-intl";
 
-//componente que exibe a imagem principal na homepage
+// Componente que exibe a imagem principal na homepage
 const Banner = () => (
-  <div id="banner-div">
-    <img src="./assets/newlogo.png" alt="Logotipo" height="320" />
-    <div className="button-container">
-      <a href="#search-bar-div" className="buybutton animate-buybutton">
-        <FormattedMessage id="buybutton.text" />
-      </a>
-      <a href="/create-product" className="sellbutton animate-sellbutton">
-        <FormattedMessage id="sellbutton.text" />
-      </a>
+  <div id="banner-div" className="container-fluid text-center py-4">
+    <div className="container">
+      <div className="row align-items-center">
+        {/* Imagem do Banner */}
+        <div className="col-12">
+          <img
+            src="./assets/newlogo.png"
+            alt="Logotipo"
+            className="img-fluid"
+            style={{ maxHeight: "320px" }}
+          />
+        </div>
+
+        {/* Botões do Banner */}
+        <div className="col-12 mt-4">
+          <div className="d-flex justify-content-center gap-3">
+            <a
+              href="#search-bar-div"
+              className="btn btn-primary buybutton animate-buybutton"
+            >
+              <FormattedMessage id="buybutton.text" />
+            </a>
+            <a
+              href="/create-product"
+              className="btn btn-danger sellbutton animate-sellbutton"
+            >
+              <FormattedMessage id="sellbutton.text" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
